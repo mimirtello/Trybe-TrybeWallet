ed25158068920d48958e1ce328b76dd38bd763dd
Yasmin Reis Tello <mimirt08@gmail.com>
2022-08-30 10:54:33 -0300

    Requisito 07

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,16 +3,11 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 class Header extends Component {
-  // state = {
-  //   total: 0,
-  //   moeda: 'BRL',
-  // };
   total = () => {
     const { expenses } = this.props;
     let total = 0;
     if (expenses) {
       expenses.forEach((elemento) => {
-        console.log(elemento);
         const valorMoeda = elemento.exchangeRates[elemento.currency].ask;
         const totalDaConta = (Number(elemento.value)) * (Number(valorMoeda));
         total += totalDaConta;
@@ -42,16 +37,14 @@ const mapStateToProps = (state) => ({
   email: state.user.email,
   value: state.wallet.value,
   expenses: state.wallet.expenses,
-  // total: state.wallet.total,
   currency: state.wallet.currency,
 });
 
 Header.propTypes = {
   email: PropTypes.string.isRequired,
-  // total: PropTypes.number.isRequired,
   currency: PropTypes.string.isRequired,
   expenses: PropTypes.arrayOf(PropTypes.object.isRequired).isRequired,
 
 };
-// Iniciando Projeto
+
 export default connect(mapStateToProps)(Header);
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Table extends Component {
   render() {
+    const { expenses } = this.props;
+
     return (
       <div>
         <table className="tabela">
-          <tbody>
+          <thead>
             <tr>
               <th>Descrição</th>
               <th>Tag</th>
@@ -17,6 +21,41 @@ class Table extends Component {
               <th>Moeda de conversão</th>
               <th>Editar/Excluir</th>
             </tr>
+          </thead>
+          <tbody>
+            { expenses.map((elemento) => (
+
+              <tr key={ elemento.id }>
+                <td>
+                  {elemento.description}
+                </td>
+                <td>
+                  {elemento.tag}
+                </td>
+                <td>
+                  {elemento.method}
+                </td>
+                <td>
+                  {Number(elemento.value).toFixed(2)}
+                </td>
+                <td>
+                  {elemento.exchangeRates[elemento.currency].name}
+                </td>
+                <td>
+                  {Number(elemento.exchangeRates[elemento.currency].ask).toFixed(2)}
+                </td>
+                <td>
+                  { Number(elemento.value * elemento
+                    .exchangeRates[elemento.currency].ask).toFixed(2) }
+                </td>
+                <td>
+                  Real
+                </td>
+
+              </tr>
+
+            )) }
+
           </tbody>
         </table>
       </div>
@@ -24,4 +63,12 @@ class Table extends Component {
   }
 }
 
-export default Table;
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+Table.propTypes = {
+  expenses: PropTypes.arrayOf(PropTypes.object.isRequired).isRequired,
+
+};
+export default connect(mapStateToProps)(Table);
